a7e69e840c40e76aaef023bdee5d6a0c6d9011fb
acruseno <aldo.cruseno@gmail.com>
2019-02-28 00:51:06 -0300

    tercera parte de creando una app con firebase - configurando AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReclamosComponent } from './reclamos/reclamos.component';
 
+import { ReclamosService } from './services/reclamos.service';
+
+import { ToastModule, ToastsManager } from 'ng2-toastr/ng2-toastr';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
     ReclamosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToastModule.forRoot(),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ 
+    ReclamosService,
+    ToastsManager
+   ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
