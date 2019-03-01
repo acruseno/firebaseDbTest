6ec1b785947df1ca9927c1f76ea199c98ce350e6
acruseno <aldo.cruseno@gmail.com>
2019-03-01 20:29:23 -0300

    definiendo metodos CRUD del servicio

--- src/app/models/reclamo.ts
+++ src/app/models/reclamo.ts
@@ -1,2 +1,7 @@
 export class Reclamo {
+    $key: string;
+    titulo: string;
+    texto: string;
+    categoria: string;
+    locacion: string;
 }
--- src/app/services/reclamos.service.ts
+++ src/app/services/reclamos.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireList } from 'angularfire2/database';
+import { Reclamo } from '../models/reclamo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReclamosService {
 
-  constructor() { }
+  reclamoList: AngularFireList<any>;
+
+  constructor(private _angularFireService) { 
+
+  }
+
+  //Obtener todos los reclamos
+  getReclamos(){
+
+  }
+
+  //retorna el reclamo solicitado
+  getReclamo(_id: any){
+
+  }
+
+  //agrega un reclamo a la BD
+  addReclamo(_reclamo: Reclamo){
+
+  }
+
+  //actualiza un registo de la base de datos
+  updateReclamo(_id: any, _reclamo: Reclamo){
+
+  }
+
+  //elimina el registro especificado de la base de datos
+  deleteReclamo(_id: any){
+
+  }
 }
