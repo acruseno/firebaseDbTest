509dd68f82fda528ea945bf864ce38bb76fc8a84
acruseno <aldo.cruseno@gmail.com>
2019-03-02 00:01:45 -0300

    implementando metodos CRUD

--- src/app/services/reclamos.service.ts
+++ src/app/services/reclamos.service.ts
@@ -16,26 +16,36 @@ export class ReclamosService {
 
   //Obtener todos los reclamos
   getReclamos(){
-
+    return this.reclamoList = this._angularFireService.list("reclamos");
   }
 
-  //retorna el reclamo solicitado
-  getReclamo(_id: any){
-
-  }
+  // //retorna el reclamo solicitado
+  // getReclamo(_id: any){
+    
+  // }
 
   //agrega un reclamo a la BD
   addReclamo(_reclamo: Reclamo){
-
+    this.reclamoList.push({
+      titulo: _reclamo.titulo,
+      texto: _reclamo.texto,
+      categoria: _reclamo.categoria,
+      locacion: _reclamo.locacion
+    });
   }
 
   //actualiza un registo de la base de datos
   updateReclamo(_id: any, _reclamo: Reclamo){
-
+    this.reclamoList.update(_id, {
+      titulo: _reclamo.titulo,
+      texto: _reclamo.texto,
+      categoria: _reclamo.categoria,
+      locacion: _reclamo.locacion
+    })
   }
 
   //elimina el registro especificado de la base de datos
   deleteReclamo(_id: any){
-
+    this.reclamoList.remove(_id);
   }
 }
